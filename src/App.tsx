72eeacd3bd2f008e74bbdf23ac435f2ede2a0943
zThiagoR <thi.magalha13@gmail.com>
2022-09-15 01:32:14 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './styles/main.css'
+
+function App() {
+
+  return (
+    <div className='h-screen flex justify-center items-center font-bold'>
+      Hello World from React + Tailwind + TypeScript + Vite!
+    </div>
+  )
+}
+
+export default App
